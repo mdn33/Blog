6f94c430aee9bc6b0bc296e523235cb441ead096
mdn33 <54626709+mdn33@users.noreply.github.com>
2021-02-13 11:47:43 +0100

    Added about page

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,23 +12,24 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
+            //var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
+            // if (firstName.indexOf(' ') >= 0) {
+            //     firstName = name.split(' ').slice(0, -1).join(' ');
+            // }
             $.ajax({
                 url: "././mail/contact_me.php",
+                //url: "C:\Users\Macco\Documents\Project_MLDL\Blog\mailcontact_me.php",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
+                    //phone: phone,
                     email: email,
                     message: message
                 },
-                cache: false,
+                //cache: false,
                 success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
@@ -47,7 +48,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName  + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
